ab805927ad8c0f5fca7d974b62ec56deb0aa753d
jrgermain <37086292+jrgermain@users.noreply.github.com>
2024-12-11 00:50:44 -0500

    Update postcss config for docs to match main sheet

--- packages/stylesheet-docs/postcss.config.cjs
+++ packages/stylesheet-docs/postcss.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
   plugins: [
     require("@csstools/postcss-sass"),
-    require("postcss-preset-env"),
-    require("cssnano"),
+    require("postcss-preset-env")({
+      features: {
+        "oklab-function": false,
+        "custom-properties": false,
+      },
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
   ],
 };
