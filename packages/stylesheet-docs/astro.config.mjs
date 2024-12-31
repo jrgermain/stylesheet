482a495e57e80c6b9f0c58f255a83fafbf170fbf
jrgermain <37086292+jrgermain@users.noreply.github.com>
2024-12-30 22:50:35 -0500

    Simplify docs setup

--- packages/stylesheet-docs/astro.config.mjs
+++ packages/stylesheet-docs/astro.config.mjs
@@ -1,19 +1,8 @@
 import { defineConfig } from "astro/config";
-import css from "rollup-plugin-import-css";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/": { destination: "/getting-started/introduction", status: 307 },
-  },
-  vite: {
-    ssr: {
-      noExternal: ["@jrgermain/stylesheet"],
-    },
-    build: {
-      rollupOptions: {
-        plugins: [css()],
-      },
-    },
   },
 });
