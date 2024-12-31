90da2406e57974a00acf455399de32538779b375
jrgermain <37086292+jrgermain@users.noreply.github.com>
2024-12-30 23:13:25 -0500

    Add rollup plugin back

--- packages/stylesheet-docs/astro.config.mjs
+++ packages/stylesheet-docs/astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
+import css from "rollup-plugin-import-css";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/": { destination: "/getting-started/introduction", status: 307 },
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        plugins: [css()],
+      },
+    },
   },
 });
