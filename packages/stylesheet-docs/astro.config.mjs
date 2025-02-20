efd00df67e15380dffcd0e4b81825a614d582730
jrgermain <37086292+jrgermain@users.noreply.github.com>
2025-02-19 23:55:22 -0500

    Refactor Tokens page as MDX

--- packages/stylesheet-docs/astro.config.mjs
+++ packages/stylesheet-docs/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/": { destination: "/getting-started/introduction", status: 307 },
   },
-  integrations: [alpinejs()],
+  integrations: [alpinejs(), mdx()],
 });
